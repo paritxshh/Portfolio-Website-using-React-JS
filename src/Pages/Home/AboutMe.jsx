bf00dc56f843a2c7fa9fd057a40203cacfa93ec8
paritxshh <paritxshh>
2023-12-17 22:36:27 +0530

    Added Skills Section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+    return (
+      <section id="AboutMe" className="about--section">
+        <div className="about--section--img">
+          <img src="./img/fotor-202312042350.png" alt="About Me" />
+        </div>
+        <div className="hero--section--content--box about--section--box">
+          <div className="hero--section--content">
+            <h1 className="skills-section--heading">About Me</h1>
+            <p className="hero--section-description">
+              Enthusiastic and motivated fresh graduate with a dual passion for Machine Learning Engineering and Front-End Development. Eager to apply foundational knowledge in both domains to tackle challenging projects and contribute to innovative solutions.
+            </p>
+            <p className="hero--section-description">
+              An adaptable team player ready to collaborate effectively in dynamic work environments, while continuously honing skills to deliver impactful results in both machine learning and front-end development.
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -49,11 +49,15 @@ function Navbar() {
                 </li>
 
                 <li>
-                    <Link onClick={closeMenu} activeClass="navbar--active-content" spy={true} smooth={true} offset={-70} duration={500} to="MyPortfolio" className="navbar--content">Portfolio</Link>
+                    <Link onClick={closeMenu} activeClass="navbar--active-content" spy={true} smooth={true} offset={-70} duration={500} to="AboutMe" className="navbar--content">About Me</Link>
                 </li>
 
                 <li>
-                    <Link onClick={closeMenu} activeClass="navbar--active-content" spy={true} smooth={true} offset={-70} duration={500} to="AboutMe" className="navbar--content">About Me</Link>
+                    <Link onClick={closeMenu} activeClass="navbar--active-content" spy={true} smooth={true} offset={-70} duration={500} to="mySkills" className="navbar--content">Expertise</Link>
+                </li>
+
+                <li>
+                    <Link onClick={closeMenu} activeClass="navbar--active-content" spy={true} smooth={true} offset={-70} duration={500} to="MyPortfolio" className="navbar--content">Portfolio</Link>
                 </li>
 
                 <li>
